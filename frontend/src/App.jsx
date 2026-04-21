4f3d9bca6ebe2c945b28c646939a1b9b134a2a90
André Barroso <andrebarroso@MacBook-Pro-Nenecos.local>
2026-04-21 01:12:34 +0100

    Adicionar Gemini

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -345,7 +345,7 @@ const AppProvider = ({ children }) => {
 
 const callGemini = async (prompt, schema) => {
   const apiKey = "AIzaSyDvuU7yOxgU8MzTw_kXcnQ31VmZsit63DY"; // Canvas environment API key, mas pode colocar a sua aqui no seu código local
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${apiKey}`;
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
 
   const payload = {
     contents: [{ parts: [{ text: prompt }] }],
